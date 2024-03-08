4239c9524f759619bce5f1e4a3718cac6915344e
etr-smlopez <smlopez@etrphils.com>
2024-03-08 14:43:46 +0800

    a

---
+++ BlazorServerApp/Hubs/UpdateHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Hubs
+{
+    public class UpdateHub : Hub
+    {
+        public async Task SendUpdate(string message)
+        {
+            await Clients.All.SendAsync("ReceiveUpdate", message);
+        }
+    }
+}
